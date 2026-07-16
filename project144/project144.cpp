fb1ef7703e325caf64f46b77bf610da0fce0f6d0
gsadmin <gsadmin@DESKTOP-R7R9J7E>
2026-07-16 17:15:25 +0700

    Final_RPG

--- project144/project144.cpp
+++ project144/project144.cpp
@@ -1,185 +1,180 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
+
 using namespace std;
 
-void Pass() {
-
-	
-	int money;
-	int honesty = 90;
-	int random = 1 + rand() % 100;
-
-	int bribePrice = 500;
-
-	string name;
-	string hasPassAnswer;
-
-	cout << "Как тебя зовут, Сталкер?\n";
-	cin >> name;
-
-	cout << "Привет." << name << "есть пропуск? (да/нет)\n";
-
-	cin >> hasPassAnswer;
-
-	if (hasPassAnswer == "Да" || hasPassAnswer == "да") {
-
-		cout << "проходи," << name << "!" << endl;
-
-	}
-	else {
-		if (random <= honesty) {
-			cout << "пропуска нет. Сколько у тебя денег?\n";
-			cin >> money;
-
-			if (money >= bribePrice) {
-				cout << "Ты заплатил " << bribePrice << " Монет. Охранник пропускает." << endl;
-
-			}
-			else {
-				cout << "не хватает денег. Охранник закрывает проход" << endl;
-
-			}
-		}
-		else {
-
-			cout << "пропуска нет, уходи.";
-		}
-
-	}
-
-}
-
-void Randomizer() {
-
-	int number;
-	int random = 1 + rand() % 100;
-	int honestran = 1 + rand() % 100;
-	for (int i = 0; i < 5;i++) {
-		int honesty = 30;
-		cout << "Введите проверочное число,чтобы потвердить личность\n";
-		cin >> number;
-		if (honestran >= honesty) {
-
-			if (number == random) {
-				cout << "Личность потверждена. Добро пожаловать!\n";
-				break;
-			}
-			else if (number < random) {
-
-				cout << "Не верно.Число больше указанного вами\n";
-
-			}
-			else if (random < number) {
-
-				cout << "Не верно.Число меньше указанного вами\n";
-			}
-		}
-		else if (honestran <= honesty) {
-
-			if (number == random) {
-				cout << "Личность потверждена. Добро пожаловать!\n";
-				break;
-			}
-			else if (number < random) {
-
-				cout << "Не верно.Число меньше указанного вами\n";
-
-			}
-			else if (random < number) {
-
-				cout << "Не верно.Число больше указанного вами\n";
-			}
-
-		}
-	
-	}
-	cout << "Число терминала: "<< random;
-
-}
+// Unit structure
+struct Unit {
+    string name;
+    int dmg;
+}; 
 
 void RPG() {
-	
-	struct Unit {
-		string name;
-		int HP;
-		int dmg;
-	};
 
-	//Иницализация данных
+    struct Ruler {
+        int HP=500;
+    };
 
-	Unit character[3];
-	character[0].name = "Sworder";
-	character[0].dmg = 10;
-	character[0].HP = 100;
+    Ruler KingPlayer;
+    Ruler KingBot;
 
-	character[1].name = "Horseman";
-	character[1].dmg = 10;
-	character[1].HP = 100;
+    const int UNIT_COUNT = 4;
+    Unit units[UNIT_COUNT];
 
-	character[2].name = "Archer";
-	character[2].dmg = 10;
-	character[2].HP = 100;
+    units[0].name = "Swordsman";
+    units[0].dmg = 100;
 
-	//Логика даных
+    units[1].name = "Horseman";
+    units[1].dmg = 100;
 
-	
-	cout << "Правила игры:\n В игре 3 юнита: Мечник, Лучник, Всадник.\nМечник силен против Лучника\nЛучник силен против Всадника.\nВсадник силен против Мечника\n\n";
-		
+    units[2].name = "Archer";
+    units[2].dmg = 100;
 
-	for (int i = 0; i < 3;i++) {
+    units[3].name = "Spearman";
+    units[3].dmg = 100;
 
-		cout << character[i].name << "\t";
-		cout << "Урон: " << character[i].dmg << "\t";
-		cout << "Здоровье: " << character[i].HP << "\n";
-	}
+    // Balance table (4x4 matrix)
+    // 1 = first unit is stronger against second unit
+    int balance[UNIT_COUNT][UNIT_COUNT] = {
+        //           Swordsman  Horseman  Archer  Spearman
+        /*Swordsman*/ {0,        0,        1,      1},    // Swordsman > Archer and Spearman
+        /*Horseman*/  {1,        0,        0,      0},    // Horseman > Swordsman
+        /*Archer*/    {0,        1,        0,      1},    // Archer > Horseman and Spearman
+        /*Spearman*/  {0,        1,        0,      0}     // Spearman > Horseman
+    };
 
-	cout << "Выберите персонажа для дальнейшей игры\n\n";
-	string name;
-	cin >> name;
-	int random = rand()% 3;
-	string bot = character[random].name;
-	cout << bot << "\n";
+    // Unit names array
+    string unitNames[UNIT_COUNT];
+    for (int i = 0; i < UNIT_COUNT; i++) {
+        unitNames[i] = units[i].name;
+    }
 
-	if (name == character[0].name && bot == character[2].name) {
+    // Show rules
+    cout << "=== GAME RULES ===" << endl;
+    cout << "4 units: Swordsman, Horseman, Archer, Spearman" << endl;
+    cout << endl;
 
-		character[0].dmg *= 1.5f;
-	}
-	else if (name == character[0].name && bot == character[1].name) {
+    // Show balance from table
+    cout << "Balance:" << endl;
+    for (int i = 0; i < UNIT_COUNT; i++) {
+        cout << units[i].name << " is strong against: ";
+        bool hasAdvantage = false;
+        for (int j = 0; j < UNIT_COUNT; j++) {
+            if (balance[i][j] == 1) {
+                cout << units[j].name << " ";
+                hasAdvantage = true;
+            }
+        }
+        if (!hasAdvantage) cout << "none";
+        cout << endl;
+    }
+    cout << endl;
 
-		character[1].dmg *= 1.5f;
-	}
-	else if (name == character[1].name && bot == character[0].name) {
+    for (int z = 0; z < 5; z++) {
 
-		character[1].dmg *= 1.5f;
-	}
+        // Show all units with stats
+        cout << "=== UNITS ===" << endl;
+        for (int i = 0; i < UNIT_COUNT; i++) {
+            cout << i + 1 << ". " << units[i].name << "\t";
+            cout << "Damage: " << units[i].dmg << endl;
+        }
+        cout << endl;
 
-	else if (name == character[2].name && bot == character[0].name) {
-		character[0].dmg *= 1.5f;
-	}
-	else if (name == character[1].name && bot == character[2].name) {
-		character[2].dmg *= 1.5f;
-	}
-	
-	else if (name == character[2].name && bot == character[1].name) {
-		character[2].dmg *= 1.5f;
-	}
+        // Player selects unit
+        cout << "Select your unit (enter number 1-4): ";
+        int playerChoice;
+        cin >> playerChoice;
+        playerChoice--; // convert to 0-based index
 
-	cout << "Урон мечника" << character[0].dmg << "\n";
-	cout << "Урон всадника" << character[1].dmg << "\n";
-	cout << "Урон лучника" << character[2].dmg << "\n";
+        if (playerChoice < 0 || playerChoice >= UNIT_COUNT) {
+            cout << "Invalid choice!" << endl;
+            return;
+        }
 
-	//Cделай дело гуляй смело
+
+        // Bot selects random unit
+        int botChoice = rand() % UNIT_COUNT;
+        cout << "Bot selected: " << botChoice + 1 << " " << units[botChoice].name << "\n\n";
+
+
+        // Check balance table to find winner
+        int playerIdx = playerChoice;
+        int botIdx = botChoice;
+
+        // Determine result using balance table
+        if (balance[playerIdx][botIdx] == 1) {
+            // Player is stronger
+            units[playerIdx].dmg *= 1.5;
+            cout << "YOUR Unit!\n " << units[playerIdx].name << " is stronger against\n "
+                << units[botIdx].name << "!" << endl;
+            cout << "Damage boosted to: " << units[playerIdx].dmg << endl;
+        }
+        else if (balance[botIdx][playerIdx] == 1) {
+            // Bot is stronger
+            units[botIdx].dmg *= 1.5;
+            cout << "BOT Unit!\n " << units[botIdx].name << " is stronger against "
+                << units[playerIdx].name << "!" << endl;
+            cout << "Bot damage boosted to: " << units[botIdx].dmg << endl;
+        }
+
+        if (units[botIdx].dmg >= KingPlayer.HP && units[playerIdx].dmg >= KingBot.HP) {
+            KingPlayer.HP -= units[botIdx].dmg;
+            KingBot.HP -= units[playerIdx].dmg;
+            KingPlayer.HP = 0;
+            KingBot.HP = 0;
+        }
+        else if (units[botIdx].dmg >= KingPlayer.HP) {
+            KingBot.HP -= units[playerIdx].dmg;
+            KingPlayer.HP = 0;
+        }
+        else if (units[playerIdx].dmg >= KingBot.HP) {
+            KingPlayer.HP -= units[botIdx].dmg;
+            KingBot.HP = 0;
+        }
+        else {
+        
+            KingPlayer.HP -= units[botIdx].dmg;
+            KingBot.HP -= units[playerIdx].dmg;
+
+        }
+
+        units[botIdx].dmg = 100;
+        units[playerIdx].dmg = 100;
+
+        cout << "Player health\t " << KingPlayer.HP << "\n";
+        cout << "Bot health\t " << KingBot.HP << "\n";
+
+
+        if (KingPlayer.HP <= 0 && KingBot.HP <= 0) {
+            cout << "DRAW! Equal strength!" << endl;
+            
+            break;
+        }
+        else if (KingPlayer.HP <= 0 || z == 5 && KingPlayer.HP < KingBot.HP) {
+            cout << "Bot win!H@H@H@";
+           
+            break;
+
+        }
+        else if (KingBot.HP <= 0 || z== 5 && KingPlayer.HP > KingBot.HP) {
+            cout << "you win!Great Job!";
+           
+            break;
+        }
+    }
+
+
+    // Проверка на итоги игры 
+    
+    // Show final stats
+    cout << endl << "=== FINAL STATS ===" << endl;
+    for (int i = 0; i < UNIT_COUNT; i++) {
+        cout << units[i].name << " - Damage: " << units[i].dmg << endl;
+    }
 }
 
-int main()  {
-	system("chcp 65001");
-	setlocale(LC_ALL, "ru_RU.UTF-8");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+int main() {
+    srand(time(0));
+    RPG();
 
-	srand(time(0));
-	//Randomizer();
-	//Pass();
-	RPG();
 }
